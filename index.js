7954be48c3eb586b16ba8b6edc4c83f3d4b3005c
sanjeev acharya <sacharya@moreyeahs.co>
2021-06-10 23:02:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const app=express();
+const PORT=process.env.PORT||4000;
+// app.use(express.static()); 
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+})
+app.listen(PORT,()=>{
+    console.log(`App Started on PORT ${PORT}`);
+})
